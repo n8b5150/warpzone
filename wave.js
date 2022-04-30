36740fed06c408daae1c883d6ff28697e846c405
Nathan Burks <87131703+n8b5150@users.noreply.github.com>
2022-04-30 12:04:45 -0400

    pseudocode

---
+++ wave.js
@@ -0,0 +1,24 @@
+//event listeners
+//gameModeOne-button
+//gameModeTwo-button
+//difficultyEasy-button
+//difficultyNormal-button
+//difficultyHard-button
+//play-button
+
+//variables
+//gameMode
+//difficulty
+
+
+//mouseover gamemode displays explanation
+
+//click gamemode, store to variable
+//difficulty slides in from right
+
+
+//click difficulty, store to variable
+//play slides in from right
+//click play
+
+//start game
